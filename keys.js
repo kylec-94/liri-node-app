29cc096447b9d8274743582128e71f0e64d03391
Kyle Crane <kylecrane@Kyles-MacBook-Pro.local>
2018-01-08 16:58:38 -0500

    added all functions

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKey = {
+  consumer_key: 'Tr8mNsx8SJirniOUAdxQeRBRh',
+  		consumer_secret: 'SHTk28PNbWn02YJcGgG4Vx2ljl2iRCB3oijmBYOVckRweBSfos',
+  		access_token_key: '950421533838774272-adCp20C3pA6W1AGOXoFMz4z7fxkUO49',
+  		access_token_secret: 't9KowxG8zFguqfTJBkFe1UgxlNYRGrcXS4FtdGasqS2eO',
+}
+
+var spotifyKey = {
+	id: 79d77384eec64b148aa9a88a6d15650f,
+  	secret: 5e0a4e6a5423479b901d8959816c3f7f
+}
+
+module.exports = spotifyKey;
+
+module.exports = twitterKey;
--- liri-commands.js
+++ liri-commands.js
@@ -6,10 +6,14 @@ var request = require('request');
 // require twitter
 
 var twitter = require('twitter');
+// require twitter API key
+// var twitterKey = require('twitterKey');
 
 // require spotify 
 
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
+// require spotify api key
+// var spotifyKey = require('spotifyKey');
 
 // console.log the process.argv 
 
@@ -25,32 +29,28 @@ var action = process.argv[2];
 
 // var for the content of the api the user wants to search
 
-var spotifyCriteria = [];
-var twitterName = [];
+// var spotifyCriteria = [];
+// var twitterName = [];
+// var movie = [];
 
 
-// take in the commands from CLI starting at position three
 
-var parameters = for (var i = 3; i < nodeArgs.length; i++) {
-	
-				spotifyCriteria.push(parameters);
-				twitterName.push(parameters);
-}
+
 
 // code a switch for the action the user chooses 
 
 switch (action) {
 
-	case "twitter";
+	case "my-tweets":
 	twitterSearch();
 	break;
 
-	case "spotify";
+	case "spotify-this-song":
 	spotifySearch();
 	break;
 
-	case "request";
-	requestSearch();
+	case "movie-this":
+	movieSearch();
 	break;
 
 }
@@ -58,24 +58,80 @@ switch (action) {
 
 // function to run criteria in twitter api
 
-function twitterSeach() {
+function twitterSearch() {
 
-	var client = new Twitter({
+	var client = new twitter({
   		consumer_key: 'Tr8mNsx8SJirniOUAdxQeRBRh',
   		consumer_secret: 'SHTk28PNbWn02YJcGgG4Vx2ljl2iRCB3oijmBYOVckRweBSfos',
   		access_token_key: '950421533838774272-adCp20C3pA6W1AGOXoFMz4z7fxkUO49',
   		access_token_secret: 't9KowxG8zFguqfTJBkFe1UgxlNYRGrcXS4FtdGasqS2eO'
 });
 
-	var screen_name
 
-	var params = {screen_name: twitterName};
+	var params = {screen_name: 'WhoisMalaa_01'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
   }
 });
 }
+
+
+
+function spotifySearch(){
+
+var trackName = process.argv[3];
+
+var spotify = new Spotify({
+  id: '79d77384eec64b148aa9a88a6d15650f',
+  secret: '5e0a4e6a5423479b901d8959816c3f7f'
+});
+ 
+spotify.search({ type: 'track', query: trackName }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+console.log("Song: " + data.tracks.items[0].name);
+console.log("Album: " + data.tracks.items[0].album.name);
+console.log("Preview URL: " + data.tracks.items[0].preview_url);
+});
+
+
+}
+
+function movieSearch() {
+
+	var movie = process.argv[3];
+
+
+	request("http://www.omdbapi.com/?apikey=trilogy&t=" + movie + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+		if (!error && response.statusCode == 200){
+			var movie = JSON.parse(body);
+
+			var results = 
+
+			"Title: " + movie.Title;
+			"Year: " + movie.Year;
+			// "Imdb Rating: " movie.imdbRating;
+			"Country: " + movie.Country
+			"Language: " + movie.Language
+			"Plot: " + movie.Plot
+			"Rotten Tomatoes Rating: " + movie.tomatoRating
+			"Rotten Tomatoes URL: " + movie.tomatoURL;
+			console.log(results);
+		} else{
+			console.log("Error: " + error);
+			return;
+		}
+	})
+}
+
+
+
+
+
 
 
 
